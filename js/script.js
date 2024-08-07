3b27ebc59bd3e600833c4e6da66ad9cc7e3b0824
Kirill <razdor.2019@mail.ru>
2024-08-07 17:17:09 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 "use strict";
 
 
-const word = 'Na\nharis';
-
-// BEGIN (write your solution here)
-console.log(word[word.length - 1])
-console.log(parseInt('12pxm', 10))
-// END
+console.log(Math.floor(12.34))
