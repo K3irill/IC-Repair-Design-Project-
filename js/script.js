67bd50d27312bc2120fe35cba562c2a741e3e22f
K3rill <razdor.2019@mail.ru>
2024-08-08 12:50:38 +0300

    learning js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 "use strict";
 
-console.log("Hello, World! sounds like shit, so i wanna print: Hello, suckers!")
+console.log(
+  "Hello, World! sounds like shit, so i wanna print: Hello, suckers!"
+);
 
-var noUse = 'it\'s var';
+var noUse = "it's var";
 console.warn(`You shouldn't use VAR to declare a variable`);
 
 let canUseIt = 131;
@@ -15,18 +17,18 @@ let isNull = null;
 
 let isUndefind = undefined;
 
-let hugeNum =  BigInt("0x1fffffffffffff");
+let hugeNum = BigInt("0x1fffffffffffff");
 
 function coolFunct() {
-    return console.error('no no im from EnGlAnd!!')
+  return console.error("no no im from EnGlAnd!!");
 }
 coolFunct();
 
-const COOL_ARRAY = ['skull', 'bones'];
+const COOL_ARRAY = ["skull", "bones"];
 
 const COOL_OBJECT = {
-    name: 'Kail',
-    age: 20,
+  name: "Kail",
+  age: 20,
 };
 
 //------------------------------------------------------------------------------------------------
@@ -56,19 +58,19 @@ console.log(remainder);
 //------------------------------------------------------------------------------------------------
 
 const showGreeting = () => {
-    const text = 'yeah, it\'s me!';
-    console.log(text);
+  const text = "yeah, it's me!";
+  console.log(text);
 };
 showGreeting();
 
 const noop = () => {
-    // Тут мог бы быть код, но его нет
+  // Тут мог бы быть код, но его нет
 };
 
 noop();
 
 const getLastChar = (str) => {
-    return str[str.length - 1];
+  return str[str.length - 1];
 };
 
 console.log(getLastChar(noUse));
@@ -77,37 +79,80 @@ console.log(noUse.replace("var", "let"));
 
 //------------------------------------------------------------------------------------------------
 
-let maxWidth = '1200px';
+let maxWidth = "1200px";
 
-const getNumWithoutStr = (str) =>{
-    return parseFloat(str)
+const getNumWithoutStr = (str) => {
+  return parseFloat(str);
 };
-console.log(getNumWithoutStr(maxWidth))
+console.log(getNumWithoutStr(maxWidth));
 
-const replace = (text, from, to) =>{
-    return text.replace(from, to);
-}
+const replace = (text, from, to) => {
+  return text.replace(from, to);
+};
 
 console.log(replace("google", "go", "mo"));
 
 //------------------------------------------------------------------------------------------------
 
-const truncate = (text, length) =>{
-    return text.slice(0, length) + '...';
-}
+const truncate = (text, length) => {
+  return text.slice(0, length) + "...";
+};
 
 //------------------------------------------------------------------------------------------------
 
-console.log(truncate("Hello, World! sounds like shit, so i wanna print: Hello, suckers!", 13));
+console.log(
+  truncate(
+    "Hello, World! sounds like shit, so i wanna print: Hello, suckers!",
+    13
+  )
+);
 
-const getHiddenCard = (card, num = 4) =>{
-    let slicerCard = card.slice(12);
-    let hiddenCard = '*'.repeat(num) + slicerCard;
-    return hiddenCard;
+const getHiddenCard = (card, num = 4) => {
+  let slicerCard = card.slice(12);
+  let hiddenCard = "*".repeat(num) + slicerCard;
+  return hiddenCard;
+};
+console.log(getHiddenCard("1234567812345678", 2));
+
+const arrowGetHiddenCard = (card, num = 4) => "*".repeat(num) + card.slice(12);
+console.log(arrowGetHiddenCard("1234567812345678", 2));
+
+const capitalize = (str) => {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+};
+console.log(capitalize("hello"));
+//--------------------------------------------------------------------------------
+
+const isStrongPassword = (password) =>{
+  const length = password.length;
+  return length >= 8 && length <= 20;
+};
+console.log(isStrongPassword('qwerty24'));
+
+const getLetter = (str, value) => {
+  if (str[value - 1]){
+    return str[value - 1]
+  }else if(str[value - 1] === undefined){
+    return '';
+  }
 }
-console.log(getHiddenCard('1234567812345678', 2));
+console.log(getLetter('Backspace', 2));
 
+function normalizeUrl(adress){
+  if(adress.startsWith("https://")){
+    return adress;
+  }else 
+    return 'https://' + adress;
+}
+console.log(normalizeUrl("google.com"))
 
-const arrowGetHiddenCard = (card, num = 4) => '*'.repeat(num) + card.slice(12);
-console.log(arrowGetHiddenCard('1234567812345678', 2));
-
+function convertText(str){
+  //  (str[0] === str[0].toUpperCase()) ? str : str.split('').reverse().join('');
+  if (str === ''){
+    return ''
+  }else if (str[0] !== str[0].toUpperCase()){
+    return str.split('').reverse().join('');
+  }else 
+    return str;
+}
+console.log(convertText('hello'))
