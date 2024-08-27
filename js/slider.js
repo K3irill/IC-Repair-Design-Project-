f60875af46b74809a637d113918d941fa43b31c8
K3rill <razdor.2019@mail.ru>
2024-08-27 21:38:20 +0300

    create slider js file

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+'use strict'
+const btnSliderBack = document.querySelector('#btnSliderBack');
+const btnSliderNext = document.querySelector('#btnSliderNext');
+const currentSlideIndex = 0;
+
+function changeSlide() {
+    
+}
+
+btnSliderBack.addEventListener('click', )
